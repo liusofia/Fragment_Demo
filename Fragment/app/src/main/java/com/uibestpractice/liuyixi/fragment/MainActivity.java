89a06496852708d9bee54aeb706622291cebfee3
liuyixi <liuyixi@xiaomi.com>
2019-02-07 21:25:00 +0800

    sofia:add dynamic Fragment demo.

---
+++ Fragment/app/src/main/java/com/uibestpractice/liuyixi/fragment/MainActivity.java
@@ -0,0 +1,46 @@
+package com.uibestpractice.liuyixi.fragment;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout);
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(this);
+        //动态添加Fragment
+        replaceFragment(new RightFragment());
+    }
+
+    //给左侧按钮注册点击事件
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.button:
+                replaceFragment(new AnotherRightFragment());
+                break;
+            default:
+                break;
+        }
+    }
+
+    //动态添加Fragment的步骤
+//1:创建待添加的碎片实例(AnotherRightFragment)
+//2:获取FragmentManager,在Activity中可以直接使用getSupportFragmentManager()方法得到
+//3:开启一个FragmentTransaction ,通过beginTransaction开启
+//4:向transaction中添加或替换Fragment,一般使用replace方法，需参数1容器id,参数2Fragment对象实例
+//5:调用commit()方法来完成。
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+        transaction.replace(R.id.right_layout,fragment);
+        transaction.commit();
+    }
+}
